e5cabda9404ef4270563bfd526898c02e4f6cb8d
Alexander <av-trifonov@yandex.ru>
2019-02-03 12:05:59 +0300

    call bmi

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # cashback
 from lib import cashback
 
-print('our programm')
-# value = cashback(10_000) # вызов + передача параметра
-# print(value)
-print(cashback(10_000)) # alt + enter
-print(cashback(20_000))
+# print('our programm')
+# # value = cashback(10_000) # вызов + передача параметра
+# # print(value)
+# print(cashback(10_000)) # alt + enter
+# print(cashback(20_000))
+
+bmi (106, 1.68)
