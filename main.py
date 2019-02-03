f67013ab28bf16d0dd25fc9bb58c4ff99aef18fe
Alexander <av-trifonov@yandex.ru>
2019-02-03 11:58:06 +0300

    Rest filse

---
+++ main.py
@@ -0,0 +1,8 @@
+# cashback
+from lib import cashback
+
+print('our programm')
+# value = cashback(10_000) # вызов + передача параметра
+# print(value)
+print(cashback(10_000)) # alt + enter
+print(cashback(20_000))
